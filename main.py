77639a41e98f6f3339ebf2a8deedeffc58e3622f
Henrique Makita <henrique_hmk@yahoo.com>
2026-07-26 18:50:30 -0300

    Remove main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from app.schema import get_schema_description
-from app.validator import validate_sql
-from app.executor import execute_sql
-
-def main():
-    print(execute_sql("SELECT * FROM FORNECEDORES"))
-
-if __name__ == "__main__":
-    main()
